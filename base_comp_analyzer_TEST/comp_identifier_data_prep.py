a8eda0cd12e9c707733532e252511daa45316a65
sanidhya-codes-he <jainsanidhya007@gmail.com>
2026-05-17 11:05:40 +0530

    added data preparation for test data

--- base_comp_analyzer_TEST/comp_identifier_data_prep.py
+++ base_comp_analyzer_TEST/comp_identifier_data_prep.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 import xgboost as xgb
 
-fan_id00_abnormal = pd.read_csv("/Listen_repo/fan_csv/fan_id00_abnormal")
-fan_id00_normal = pd.read_csv("/Listen_repo/fan_csv/fan_id00_normal")
-fan_id02_abnormal = pd.read_csv("/Listen_repo/fan_csv/fan_id02_abnormal")
-fan_id02_normal = pd.read_csv("/Listen_repo/fan_csv/fan_id02_normal")
-fan_id04_abnormal = pd.read_csv("/Listen_repo/fan_csv/fan_id04_abnormal")
-fan_id04_normal = pd.read_csv("/Listen_repo/fan_csv/fan_id04_normal")
-fan_id06_abnormal = pd.read_csv("/Listen_repo/fan_csv/fan_id06_abnormal")
-fan_id06_normal = pd.read_csv("/Listen_repo/fan_csv/fan_id06_normal")
+fan_id_00_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id00_abnormal")
+fan_id_00_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id00_normal")
+fan_id_02_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id02_abnormal")
+fan_id_02_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id02_normal")
+fan_id_04_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id04_abnormal")
+fan_id_04_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id04_normal")
+fan_id_06_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id06_abnormal")
+fan_id_06_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\fan_csv\\fan_id06_normal")
 
 pump_id_00_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\pump_csv\\pump_id_00_abnormal")
 pump_id_00_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\pump_csv\\pump_id_00_normal")
@@ -29,14 +29,14 @@ slider_id_04_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Li
 slider_id_06_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\slider_csv\\slider_id_06_abnormal")
 slider_id_06_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\slider_csv\\slider_id_06_normal")
 
-valve_id_00_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_00_abnormal")
-valve_id_00_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_00_normal")
-valve_id_02_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_02_abnormal")
-valve_id_02_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_02_normal")
-valve_id_04_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_04_abnormal")
-valve_id_04_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_04_normal")
-valve_id_06_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_06_abnormal")
-valve_id_06_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id_06_normal")
+valve_id_00_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id00_abnormal")
+valve_id_00_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id00_normal")
+valve_id_02_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id02_abnormal")
+valve_id_02_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id02_normal")
+valve_id_04_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id04_abnormal")
+valve_id_04_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id04_normal")
+valve_id_06_abnormal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id06_abnormal")
+valve_id_06_normal = pd.read_csv("C:\\Users\\sanid\\PycharmProjects\\Listen\\Listen_repo\\valve_csv\\valve_id06_normal")
 
 valve = pd.concat([valve_id_04_abnormal,valve_id_04_normal,valve_id_02_abnormal,valve_id_02_normal,valve_id_06_abnormal,valve_id_06_normal,valve_id_00_abnormal,valve_id_00_normal],ignore_index=True)
 
@@ -44,13 +44,13 @@ slider = pd.concat([slider_id_04_abnormal,slider_id_04_normal,slider_id_02_abnor
 
 pump = pd.concat([pump_id_04_abnormal,pump_id_04_normal,pump_id_02_abnormal,pump_id_02_normal,pump_id_06_abnormal,pump_id_06_normal,pump_id_00_abnormal,pump_id_00_normal],ignore_index=True)
 
-fan = pd.concat([fan_id04_abnormal,fan_id04_normal,fan_id02_abnormal,fan_id02_normal,fan_id06_abnormal,fan_id06_normal,fan_id00_abnormal,fan_id00_normal],ignore_index=True)
+fan = pd.concat([fan_id_04_abnormal,fan_id_04_normal,fan_id_02_abnormal,fan_id_02_normal,fan_id_06_abnormal,fan_id_06_normal,fan_id_00_abnormal,fan_id_00_normal],ignore_index=True)
 
 valve['comp_id'] = 0
 slider['comp_id'] = 1
 pump['comp_id'] = 2
 fan['comp_id'] = 3
 
-print(fan)
+data = pd.concat([valve,slider,pump,fan],ignore_index=True)
 
 
