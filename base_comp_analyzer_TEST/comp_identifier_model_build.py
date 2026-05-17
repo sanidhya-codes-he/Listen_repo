c5f18cfa8ed10096f7bec8c90e613229fc2562b8
sanidhya-codes-he <jainsanidhya007@gmail.com>
2026-05-17 11:14:56 +0530

    added model  build file

---
+++ base_comp_analyzer_TEST/comp_identifier_model_build.py
@@ -0,0 +1,6 @@
+from comp_identifier_data_prep import data
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
